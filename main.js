02182529e903ff3a9e3db947580b056361e89091
Joel Hjelmstedt <joelbeats@gmail.com>
2020-11-27 23:01:40 +0100

    Added functions for LS in main.js and added <ul> in index.html

--- main.js
+++ main.js
@@ -38,16 +38,49 @@ let quill = new Quill('#editor', {
 });
 
 let noteObject = {};
+let noteObjectAsString = '';
+let saveButton = document.querySelector('#saveBtn');
 
-var saveButton = document.querySelector("#saveBtn");
+const NOTE_INDEX_VALUE = 'numberOfNotesInLocalStorage'
+/*
+if(localStorage.getItem(NOTE_INDEX_VALUE)) {
+  console.log('Det finns noteIndexValue i LS', localStorage.getItem(NOTE_INDEX_VALUE))
+}else {
+  console.log('det finns ingen noteIndexValue')
+  localStorage.setItem(NOTE_INDEX_VALUE, 0)
+}
+*/
+function readAllNotes(){
+  const currentNoteIndexValueAsString = localStorage.getItem(NOTE_INDEX_VALUE)
+  const currentNoteIndexValueAsInteger = parseInt(currentNoteIndexValueAsString)
+  const allNotes = []
+  for( let i = 0; i < currentNoteIndexValueAsInteger; i++){
+    let noteKey = 'note-' + i
+    const objectInLocalStorageAsString = localStorage.getItem(noteKey)
+    const objectInLocalStorage = JSON.parse(objectInLocalStorageAsString)
+    allNotes.push(objectInLocalStorage)
+  }
+  return allNotes
+}
+
+function updateNoteIndexValueWithOne(){
+  const currentNoteIndexValueAsString = localStorage.getItem(NOTE_INDEX_VALUE)
+  const currentNoteIndexValueAsInteger = parseInt(currentNoteIndexValueAsString)
+  const nextNoteIndexValue = currentNoteIndexValueAsInteger + 1
+  localStorage.setItem(NOTE_INDEX_VALUE, nextNoteIndexValue)
+}
+
 saveButton.addEventListener('click', function() {
+  
+  // definiera notekey som string
+  let noteKey = 'note-' + localStorage.getItem(NOTE_INDEX_VALUE)
   // lägg till content i ett delta objekt
   window.delta = quill.getContents();
   quill.setContents(window.delta);
   // sätter delta objektet till det globala objektet
   noteObject = window.delta;
   // gör om objektet till en sträng som kan sparas i LS
-  let noteObject_string = JSON.stringify(noteObject)
-  localStorage.setItem("note", noteObject_string)
-
+  noteObjectAsString = JSON.stringify(noteObject)
+  localStorage.setItem(noteKey, noteObjectAsString)
+  updateNoteIndexValueWithOne()
 });
