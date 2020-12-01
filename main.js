2f305df99923b48e69919849d31f59f18a505163
Joel Hjelmstedt <joelbeats@gmail.com>
2020-12-01 15:50:04 +0100

    fixed js

--- main.js
+++ main.js
@@ -37,55 +37,7 @@ let quill = new Quill('#editor', {
   theme: 'snow'  // or 'bubble'
 });
 
-<<<<<<< Updated upstream
-let noteObject = {};
-let noteObjectAsString = '';
-let saveButton = document.querySelector('#saveBtn');
 
-const NOTE_INDEX_VALUE = 'numberOfNotesInLocalStorage'
-/*
-if(localStorage.getItem(NOTE_INDEX_VALUE)) {
-  console.log('Det finns noteIndexValue i LS', localStorage.getItem(NOTE_INDEX_VALUE))
-}else {
-  console.log('det finns ingen noteIndexValue')
-  localStorage.setItem(NOTE_INDEX_VALUE, 0)
-}
-*/
-function readAllNotes(){
-  const currentNoteIndexValueAsString = localStorage.getItem(NOTE_INDEX_VALUE)
-  const currentNoteIndexValueAsInteger = parseInt(currentNoteIndexValueAsString)
-  const allNotes = []
-  for( let i = 0; i < currentNoteIndexValueAsInteger; i++){
-    let noteKey = 'note-' + i
-    const objectInLocalStorageAsString = localStorage.getItem(noteKey)
-    const objectInLocalStorage = JSON.parse(objectInLocalStorageAsString)
-    allNotes.push(objectInLocalStorage)
-  }
-  return allNotes
-}
-
-function updateNoteIndexValueWithOne(){
-  const currentNoteIndexValueAsString = localStorage.getItem(NOTE_INDEX_VALUE)
-  const currentNoteIndexValueAsInteger = parseInt(currentNoteIndexValueAsString)
-  const nextNoteIndexValue = currentNoteIndexValueAsInteger + 1
-  localStorage.setItem(NOTE_INDEX_VALUE, nextNoteIndexValue)
-}
-
-saveButton.addEventListener('click', function() {
-  
-  // definiera notekey som string
-  let noteKey = 'note-' + localStorage.getItem(NOTE_INDEX_VALUE)
-  // lägg till content i ett delta objekt
-  window.delta = quill.getContents();
-  quill.setContents(window.delta);
-  // sätter delta objektet till det globala objektet
-  noteObject = window.delta;
-  // gör om objektet till en sträng som kan sparas i LS
-  noteObjectAsString = JSON.stringify(noteObject)
-  localStorage.setItem(noteKey, noteObjectAsString)
-  updateNoteIndexValueWithOne()
-});
-=======
 const inputTitle = document.querySelector("#inputTitle") //det som skrivs in i input rutan sparas i konstanten inputTitle
 const saveBtn = document.querySelector("#saveBtn") //vi tilldelar knappen en konstant för att kunna göra det nedre steget
 saveBtn.addEventListener('click', localSave) //sidan "lyssnar"/läser av när vi klickar på knappen och vi vill skicka det vi sparar till localSave
@@ -110,4 +62,3 @@ function localSaveEditor() {
   
 }
 
->>>>>>> Stashed changes
