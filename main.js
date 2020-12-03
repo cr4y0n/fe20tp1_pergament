8a19caa2d16870fd7e2e5e39410a1ca8d354269f
Joel Hjelmstedt <joelbeats@gmail.com>
2020-12-04 00:25:51 +0100

    some more cheanges

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let startBtn = document.querySelector('#startBtn')
 let gotchaBtn = document.querySelector('#gotchaBtn')
 let homeLink = document.querySelector('#homeLink')
+let newBtn = document.querySelector('#newBtn')
 const titlePage = document.querySelector('#titlePage')
 const howItWorksPage = document.querySelector('#howItWorks')
 
 startBtn.addEventListener('click', hideTitlePage)
 homeLink.addEventListener('click', showTitlePage)
 gotchaBtn.addEventListener('click', linkToApp)
+newBtn.addEventListener('click', linkToApp)
 
 function hideHowItWorks() {
   howItWorksPage.classList.add('hide-me')
